2523b6538e2008f9877783c797332692b3966006
Guifeng Wei <guifengwei@gmail.com>
2017-03-09 11:15:50 +0000

    Update Split.sh

--- Split.sh
+++ Split.sh
@@ -9,7 +9,7 @@ do
     samtools view -@ 8 $sample/${sample}.bam -o $sample/${sample}.sam
     samtools view -@ 8 $sample/${sample}.genome1.bam -o $sample/${sample}.genome1.sam
     samtools view -@ 8 $sample/${sample}.genome2.bam -o $sample/${sample}.genome2.sam
-	htseq-count -s reverse -t transcript $sample/${sample}.sam ~/Project/mm10/Annotation/archive-2014-05-23-16-05-10/Genes/Genes.For4sU.htseq.gtf > $sample/${sample}.counts
-	 htseq-count -s reverse -t transcript $sample/${sample}.genome1.sam ~/Project/mm10/Annotation/archive-2014-05-23-16-05-10/Genes/Genes.For4sU.htseq.gtf > $sample/${sample}.genome1.counts
-	 htseq-count -s reverse -t transcript $sample/${sample}.genome2.sam ~/Project/mm10/Annotation/archive-2014-05-23-16-05-10/Genes/Genes.For4sU.htseq.gtf > $sample/${sample}.genome2.counts
+    htseq-count -s reverse -t transcript $sample/${sample}.sam ~/Project/mm10/Annotation/archive-2014-05-23-16-05-10/Genes/Genes.For4sU.htseq.gtf > $sample/${sample}.counts
+    htseq-count -s reverse -t transcript $sample/${sample}.genome1.sam ~/Project/mm10/Annotation/archive-2014-05-23-16-05-10/Genes/Genes.For4sU.htseq.gtf > $sample/${sample}.genome1.counts
+    htseq-count -s reverse -t transcript $sample/${sample}.genome2.sam ~/Project/mm10/Annotation/archive-2014-05-23-16-05-10/Genes/Genes.For4sU.htseq.gtf > $sample/${sample}.genome2.counts
 done
